eec9d68521169cda47a66c6a46dcbe3e3e6a0431
Jerry Fan <huanyueyaoqin@qq.com>
2024-01-27 15:19:18 +0800

    🔧 chore: 修改dumi配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -19,4 +19,9 @@ export default defineConfig({
   outputPath: 'docs-dist',
   npmClient: 'pnpm',
   themeConfig: themeConfig,
+  apiParser: {},
+  resolve: {
+    // 配置入口文件路径，API 解析将从这里开始
+    entryFile: './src/index.tsx',
+  },
 });
